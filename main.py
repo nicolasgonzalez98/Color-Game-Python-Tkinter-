01244fa1399328a73aaef837287ae9bb4606155a
unknown <nicolasgonzalez470@gmail.com>
2022-03-26 13:38:02 -0300

    need a fix

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 '''
 Imports
 '''
+from cgitb import text
 import tkinter as tk
-import random
+import random as rn
 
 ##Lista de colores
 
 colores={
-    'Rojo':'Red','Azul':'Blue','Verde':'Green',
-    'Rosa':'Pink','Negro':'Black','Amarillo':'Yellow','Naranja':'Orange',
-    'Blanco':'White','Violeta':'Purple','Marron':'Brown'
+    'rojo':'Red','azul':'Blue','verde':'Green',
+    'rosa':'Pink','negro':'Black','amarillo':'Yellow','naranja':'Orange',
+    'blanco':'White','violeta':'Purple','marron':'Brown'
 }
 
 ##Variables globales
@@ -31,10 +32,29 @@ def countdown():
         timeLabel.config(text = f"Time left: {timeleft}")
         timeLabel.after(1000, countdown)
 
+def cambiar_color():
+    global score
+    global timeleft
+
+    color_esp = list(colores.keys())
+    
+    if timeleft>0:
+        input.focus_set()
+        color_palabra ='rojo'##color_esp[rn.randint(0,len(color_esp)-1)]
+        color_letra =colores[color_esp[rn.randint(0,len(color_esp)-1)]]
+        labelColor.config(text=color_palabra, fg = color_letra)
+        input_value = input.get()
+        if(colores[input_value] == color_letra):
+            score += 1
+        scoreLabel.config(text=f'Puntaje: {score}')
+        input.delete(0, tk.END)
+        
+    
+
 def start_game(event):
     if timeleft == 30:
         countdown()
-
+    cambiar_color()
 
 #Inicio de ventana
 root = tk.Tk()
@@ -44,13 +64,25 @@ root.title("COLORGAME")
 etiqueta = tk.Label(text="Escribí el color de las letras. No el color de las palabras:")
 etiqueta.place(x=2,y=10)
 
+##Label del color
+labelColor = tk.Label(root,font = ('Helvetica', 60))
+labelColor.place(x=3,y=100)
+
+##INPUT
 input = tk.Entry()
 input.place(x = 85, y = 250)
+input.focus_set()
 
 ##Temporizador
 stringTiempo = f'Time left: {timeleft}'
 timeLabel = tk.Label(text=stringTiempo)
 timeLabel.place(x=110,y=60)
+##Puntaje
+stringPuntaje = f'Puntaje: {score}'
+scoreLabel = tk.Label(text=stringPuntaje)
+scoreLabel.place(x=110,y=30)
+
+
 
 ##Eventos(Al apretar enter)
 root.bind('<Return>', start_game)
